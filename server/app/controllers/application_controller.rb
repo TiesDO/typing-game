943f57df7f7934e87ce1a073700e8f1a670dc5fa
TiesDO <denouden.ties@gmail.com>
2023-03-15 00:06:50 +0100

    add jwt like token helpers

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,18 +1,40 @@
+require 'digest'
+require 'base64'
+require 'json'
+
 class ApplicationController < ActionController::API
 
-# TODO: Parse a given custom jwt like token from the Authorization header
 def get_token
-  request.headers['Authorization']
+  head = request.headers['Authorization']
+
+  return nil if head.nil?
+
+  # parse the header value
+  split_head = head.split
+
+  return nil if split_head[0] != 'Bearer'
+  return nil if !validate_token(split_head[1])
+  
+  return JSON.parse Base64.urlsafe_decode(token.split('.')[0])
 end
 
-# TODO: Validate the users token
-def validate_token
-
+def validate_token(token)
+  payload = token.split('.')[0]
+  return sign_token(payload) === token
 end
 
-# TODO: Sign a users token
-def sign_token
+def sign_token(payload)
+  salt_token = "mysecretsalt"
 
+  payload = Base64.urlsafe_encode payload, false # use encoded payload inside signature
+
+  hash = Digest::MD5.new
+  hash << payload
+  hash << salt_token
+
+  signature = Base64.urlsafe_encode(hash.hexdigest, false)
+
+  return "#{payload}.#{signature}"
 end
 
 end
