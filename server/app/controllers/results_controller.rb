f26ac69cb16eaafe6f4363439df18df5fdd708ad
TiesDO <denouden.ties@gmail.com>
2023-03-15 01:56:58 +0100

    add result actions
    - add leaderboard
    - add results posting

--- server/app/controllers/results_controller.rb
+++ server/app/controllers/results_controller.rb
@@ -3,25 +3,94 @@ class ResultsController < ApplicationController
   def index
     # check if the user is logged in
     user = get_token
-    render json: { status: 'ERROR', message: 'no user token provided' }, status: '401' if user.nil?
+    if user.nil?
+      return render json: {
+        status: 'ERROR',
+        message: 'no user token provided'
+      }, status: '401'
+    end
 
     # check if an id is provided
-    render json: { status: 'ERROR', message: 'no valid resource id provided' }, status: '400' if param[:id].nil?
+    if param[:id].nil?
+      return render json: {
+        status: 'ERROR',
+        message: 'no valid resource id provided'
+      }, status: '400'
+    end
 
     # locate the requested resource
     result = Result.where(id: params[:id])
-    render json: { status: 'ERROR', message: 'no resource found' }, status: '404' if result.nil?
+    if result.nil?
+      return render json: {
+        status: 'ERROR',
+        message: 'no resource found'
+      }, status: '404'
+    end
 
     # validate the access to the resource
-    render json: { status: 'ERROR', message: 'access denied' }, status: '401' if result[0].user_id != user.id
+    if result[0].user_id != user.id
+      return render json: {
+        status: 'ERROR',
+        message: 'access denied'
+      }, status: '401'
+    end
 
     # return the result
-    render json: { status: 'SUCCESS', message: 'Result found', data: result }
+    render json: {
+      status: 'SUCCESS',
+      message: 'Result found',
+      data: result
+    }
   end
 
   # TODO: Show the top 10 results
-  def leaderboard; end
+  def leaderboard
+    top = Result.all.order(cpm: :asc).limit(10)
 
-  # TODO: Add a new result
-  def create; end
+    output = []
+    top.each do |r|
+      output.push({ cpm: r['cpm'],
+                    wpm: r['wpm'],
+                    time: r['timeElapsed'],
+                    user_id: r['user_id'],
+                    username: r.user.username
+                  })
+    end
+
+    render json: { data: output }
+  end
+
+  def create
+    user = get_token
+    if user.nil?
+      return render json: {
+        status: 'ERROR',
+        message: 'no user token provided'
+      }, status: '401'
+    end
+
+    body = JSON.parse(request.body.read)
+
+    # WARN: not validating input
+
+    Result.create({
+                    cpm: body['cpm'],
+                    wpm: body['wpm'],
+                    correct_cpm: body['correctCpm'],
+                    time_elapsed: body['timeElapsed'],
+                    final_mistakes: body['finalMistakes'],
+                    total_mistakes: body['totalMistakes'],
+                    total_words: body['totalWords'],
+                    total_characters: body['totalCharacters'],
+                    user_id: user['id']
+                  })
+
+    all = Result.where(user_id: user['id'])
+
+    render json: {
+      status: 'SUCCESS',
+      message: 'Result created successfully',
+      data: all
+    }, status: '201'
+  end
 end
